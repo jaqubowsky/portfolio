144a55a559eb878a1a926b51231fc717b36976d2
Jakub Nalewajk <jaqubowskyy@gmail.com>
2024-08-11 16:17:10 +0200

    Add footer with social media

--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -2,3 +2,4 @@ export { default as Contact } from "./Contact.astro";
 export { default as Header } from "./Header.astro";
 export { default as Home } from "./Home.astro";
 export { default as Skills } from "./Skills.astro";
+export { default as Footer } from "./Footer.astro";
