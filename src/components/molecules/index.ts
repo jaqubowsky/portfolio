516c9666e5aaa2ce4ef5d173e251e6733ac805aa
Jakub Nalewajk <jaqubowskyy@gmail.com>
2024-08-18 14:41:51 +0200

    Add pagination, fancy buttons, tags, improve styling

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,4 +1,6 @@
+export { default as BackBlogButton } from "./IconAnimatedButton.astro";
 export { default as Navigation } from "./Navigation.astro";
 export { default as SkillCard } from "./SkillCard.astro";
 export { default as SocialMedia } from "./SocialMedia.astro";
 export { default as TableOfContents } from "./TableOfContents.astro";
+
