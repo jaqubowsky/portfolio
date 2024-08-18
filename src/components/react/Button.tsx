eb7000591c6221c03a202fab0ff26cc515e59720
Jakub Nalewajk <jaqubowskyy@gmail.com>
2024-08-18 22:18:23 +0200

    Fix styling and images loading

--- src/components/react/Button.tsx
+++ src/components/react/Button.tsx
@@ -5,8 +5,8 @@ type Props = {
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 const variants = {
-  primary: "bg-primary   text-primary-foreground   ",
-  secondary: "  bg-secondary   text-secondary-foreground ",
+  primary: "bg-primary hover:bg-primary/80 text-primary-foreground",
+  secondary: "bg-secondary hover:bg-secondary/80 text-secondary-foreground",
 } as const;
 
 const Button = ({
@@ -18,7 +18,7 @@ const Button = ({
   return (
     <button
       className={cn(
-        "py-2 cursor-pointer border hover:bg-opacity-70  :opacity-70 inline-flex h-10 items-center justify-center rounded-md px-8 text-sm font-medium shadow transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring allowed disabled:pointer-events-none disabled:opacity-50",
+        "py-2 cursor-pointer inline-flex h-10 items-center justify-center rounded-md px-8 text-sm font-medium shadow transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring allowed disabled:pointer-events-none disabled:opacity-50",
         variants[variant],
         className,
       )}
