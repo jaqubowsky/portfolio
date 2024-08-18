f70c6fddbb7cd4214065ae4b38a7fb24293c8d38
Jakub Nalewajk <jaqubowskyy@gmail.com>
2024-08-18 19:31:31 +0200

    Add hamburger menu, fix scripts loading after page transitions

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -4,3 +4,4 @@ export { default as SkillCard } from "./SkillCard.astro";
 export { default as SocialMedia } from "./SocialMedia.astro";
 export { default as TableOfContents } from "./TableOfContents.astro";
 export { default as ThemeToggler } from "./ThemeToggler.astro";
+export { default as Hamburger } from "./Hamburger.astro";
