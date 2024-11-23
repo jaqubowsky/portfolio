9f438a2bc88768df5b743e18b028e094595d1a45
Jakub Nalewajk <jaqubowskyy@gmail.com>
2024-11-23 15:52:47 +0100

    feat: add new blog post

--- src/content/config.ts
+++ src/content/config.ts
@@ -7,7 +7,7 @@ const postsCollection = defineCollection({
     pubDate: z.date(),
     description: z.string(),
     author: z.string(),
-    category: z.enum(["blog", "news", "tutorial"]),
+    category: z.enum(["blog", "news", "tutorial", "programming"]),
     draft: z.boolean(),
     image: z.object({
       url: z.string(),
