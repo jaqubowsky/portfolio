53df0c83def0f341a8f01c29bcf24dbb1d48c0e8
Jakub Nalewajk <jaqubowskyy@gmail.com>
2026-03-11 00:56:52 +0100

    fix: replace Image with Picture component, add new images, and update image paths for blog posts

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,19 +4,20 @@ import { z } from "astro/zod";
 
 const postsCollection = defineCollection({
   loader: glob({ pattern: "**/*.mdx", base: "./src/content/posts" }),
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    description: z.string(),
-    author: z.string(),
-    category: z.enum(["blog", "news", "tutorial", "programming"]),
-    draft: z.boolean(),
-    image: z.object({
-      url: z.string(),
-      alt: z.string(),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      pubDate: z.date(),
+      description: z.string(),
+      author: z.string(),
+      category: z.enum(["blog", "news", "tutorial", "programming"]),
+      draft: z.boolean(),
+      image: z.object({
+        url: image(),
+        alt: z.string(),
+      }),
+      tags: z.array(z.string()),
     }),
-    tags: z.array(z.string()),
-  }),
 });
 
 export const collections = {
